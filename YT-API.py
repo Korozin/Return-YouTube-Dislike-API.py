129015f25ea2c5b05792191e50359c4162738e98
Korozin <90534409+Korozin@users.noreply.github.com>
2022-08-21 23:31:52 -0400

    Cleaned up to remove un-needed vars.

--- YT-API.py
+++ YT-API.py
@@ -7,24 +7,17 @@ text = url.text
 
 data = json.loads(text)
 
-dateCreated = data['dateCreated']
-likes = data['likes']
-dislikes = data['dislikes']
-rating = data['rating']
-viewCount = data['viewCount']
-deleted = data['deleted']
-
 print("\n-----------------------------")
 print("YouTube Dislike API Script by: KorOwOzin")
 print("-----------------------------\n")
 
 if "id" in data:
   print(f'Video URL: youtube.com/watch?v={vidCode}')
-  print(f'Date Created: {dateCreated}')
-  print(f'Likes: {likes}')
-  print(f'Dislikes: {dislikes}')
-  print(f'Rating: {rating}')
-  print(f'ViewCount: {viewCount}')
-  print(f'Is Video Deleted?: {deleted}\n')
+  print('Date Created: '+ data['dateCreated'])
+  print('Likes: '+ (str(data['likes'])))
+  print('Dislikes: '+ (str(data['dislikes'])))
+  print('Rating: '+ (str(data['rating'])))
+  print('ViewCount: '+ (str(data['viewCount'])))
+  print('Is Video Deleted?: '+ (str(data['deleted']))+'\n')
 else:
   print("The video could not be found.\n")
